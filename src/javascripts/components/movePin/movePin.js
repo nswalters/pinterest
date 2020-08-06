acdc1b3de263372106aefc824e2b07a008761071
Nick Walters <nswalters.s@gmail.com>
2020-08-06 18:18:10 -0500

    Updates join table record to move pin between boards

--- src/javascripts/components/movePin/movePin.js
+++ src/javascripts/components/movePin/movePin.js
@@ -3,31 +3,24 @@ import 'firebase/auth';
 import boardData from '../../helpers/data/boardData';
 import utils from '../../helpers/utils';
 import boardPinData from '../../helpers/data/boardPinData';
-// import boardPinData from '../../helpers/data/boardPinData';
+import singleBoard from '../singleBoard/singleBoard';
 
 const movePinEvent = (e) => {
-  const selectedBoard = $('.custom-select').children('option:selected').val();
   const selectedBoardId = $('.custom-select').children('option:selected').attr('data-new-board-id');
-  console.error('SelectedBoard: ', selectedBoard);
-  console.error('SelectedBoardId: ', selectedBoardId);
-
   const boardUid = e.target.closest('.form-move-pin-btn').getAttribute('data-boardid');
   const pinId = e.target.closest('.form-move-pin-btn').getAttribute('data-pinid');
 
   const newBoardPinObj = {
-    boardUid,
+    boardUid: selectedBoardId,
     pinId,
   };
 
-  boardPinData.getBoardPinIdByBoardUid(selectedBoardId)
+  boardPinData.getBoardPinIdByBoardUid(boardUid)
     .then((boardPinId) => {
       boardPinData.updateBoardPins(boardPinId, newBoardPinObj)
         .then(() => {
-          console.error('call to getBoardIdByPinId: ', boardPinId);
-          console.error(newBoardPinObj);
-          console.error('UpdatedBoardPins');
-        })
-        .catch((err) => console.error(err));
+          singleBoard.buildBoard(e);
+        });
     })
     .catch((err) => console.error('error', err));
 };
@@ -36,12 +29,9 @@ const showMovePin = (e) => {
   const userId = firebase.auth().currentUser.uid;
   const boardId = e.target.closest('.move-pin').getAttribute('data-boardid');
   const pinId = e.target.closest('.move-pin').getAttribute('data-pinid');
-  console.error('boardId: ', boardId);
-  console.error('pinId: ', pinId);
 
   boardData.getBoards()
     .then((boards) => {
-      console.error(boards);
       let domString = `
       <h2>Move to Board</h2>
       <div class="input-group">
@@ -51,7 +41,6 @@ const showMovePin = (e) => {
       boards.forEach((board) => {
         if (board.id !== boardId) {
           if (userId === board.userId) {
-            console.error(board.id);
             domString += `
             <option data-new-board-id="${board.id}" value="${board.name}">${board.name}</option>
             `;
