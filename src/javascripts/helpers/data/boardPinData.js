1163b864139312a8605b7d6799a8fb606d896a64
Nick Walters <nswalters.s@gmail.com>
2020-08-06 18:22:14 -0500

    Adds functionality to get a board id by a given pin id; Get join table record by given boardUid; Update join table record when given a join table id and an updated object

--- src/javascripts/helpers/data/boardPinData.js
+++ src/javascripts/helpers/data/boardPinData.js
@@ -31,15 +31,40 @@ const getBoardPinsByPinId = (pinId) => new Promise((resolve, reject) => {
     .catch((err) => reject(err));
 });
 
+const getBoardIdByPinId = (pinId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/boardPins.json?orderBy="pinId"&equalTo="${pinId}"`)
+    .then((response) => {
+      const boardObj = response.data;
+      const boardPinId = Object.keys(boardObj)[0];
+      resolve(boardPinId);
+    })
+    .catch((err) => reject(err));
+});
+
+const getBoardPinIdByBoardUid = (boardUid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/boardPins.json?orderBy="boardUid"&equalTo="${boardUid}"`)
+    .then((response) => {
+      const boardObj = response.data;
+      const boardPinId = Object.keys(boardObj)[0];
+      resolve(boardPinId);
+    })
+    .catch((err) => reject(err));
+});
+
 // Delete Board Pins Record
 const deleteBoardPins = (boardPinsId) => axios.delete(`${baseUrl}/boardPins/${boardPinsId}.json`);
 
 // Add new board pins record
 const addBoardPins = (boardPinsObj) => axios.post(`${baseUrl}/boardPins.json`, boardPinsObj);
 
+const updateBoardPins = (boardPinId, boardPinsObj) => axios.put(`${baseUrl}/boardPins/${boardPinId}.json`, boardPinsObj);
+
 export default {
+  getBoardIdByPinId,
   getBoardPinsByBoardId,
   getBoardPinsByPinId,
+  getBoardPinIdByBoardUid,
   deleteBoardPins,
   addBoardPins,
+  updateBoardPins,
 };
